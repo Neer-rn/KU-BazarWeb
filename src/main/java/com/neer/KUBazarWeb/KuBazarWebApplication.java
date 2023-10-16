332e6ce5ee721c8d11d201941c8e5c98e33cd6ec
Neer-rn <neerrnbhatta@gmail.com>
2023-10-16 12:32:28 +0545

    Initial commit

---
+++ src/main/java/com/neer/KUBazarWeb/KuBazarWebApplication.java
@@ -0,0 +1,13 @@
+package com.neer.KUBazarWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuBazarWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuBazarWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neer/KUBazarWeb/KuBazarWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neer.KUBazarWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuBazarWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
