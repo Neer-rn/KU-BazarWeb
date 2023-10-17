121f15a0f8adc5f1d53dd9ade809ecaa726ae189
Neer-rn <neerrnbhatta@gmail.com>
2023-10-17 23:25:44 +0545

    Firebase Auth

---
+++ src/main/java/com/neer/KUBazarWeb/firebase/firebaseAuth.java
@@ -0,0 +1,34 @@
+package com.neer.KUBazarWeb.firebase;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Objects;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class firebaseAuth {
+
+public static void main(String[] args) {
+
+        ClassLoader classLoader = firebaseAuth.class.getClassLoader();
+        File file = new File(Objects.requireNonNull(classLoader.getResource("serviceKey.json")).getFile());
+
+        try {
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+        FirebaseOptions options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+        .build();
+        FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+        System.out.println("An error occurred while fetching the service key.");
+        System.out.println(e.getMessage());
+        }
+
+        }
+
+        }
