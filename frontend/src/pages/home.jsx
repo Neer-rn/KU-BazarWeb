9809011b94c645dce3da631063bc5d7f603296cb
Neer-rn <neerrnbhatta@gmail.com>
2023-10-20 11:38:23 +0545

    nav bar made

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -4,10 +4,16 @@ import "./home.css";
 const Home = () => {
     return (
         <div className="Home">
-            <div className="home_logo">
+            <div className="home_name">
                 <h1>KU-Bazar</h1>
 
             </div>
+            <div className="home_list">
+                <li>Shop</li>
+                <li>About us</li>
+                <li>Contact us</li>
+            </div>
+                <button className="button_signup">Sign up</button>    
         </div>
     );
 }
