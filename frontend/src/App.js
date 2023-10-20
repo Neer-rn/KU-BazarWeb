b39122d9c0a5a1f58f506019e1d7293d1b5ef035
Neer-rn <neerrnbhatta@gmail.com>
2023-10-20 22:40:46 +0545

    sign up page basic made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
-import Home from './pages/home.jsx';
+import Login from './pages/login.jsx'; 
 
 function App() {
   return (
     <div className="App">
       
-      <Home/>
-      
+     <Login/>
 
     </div>
   );
---
+++ frontend/src/pages/login.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./login.css";
+
+const Login = () => {
+    return (
+        <div className="Login">
+            <div className="Up_bar">
+
+            <div className="home_list">
+                <li>Help</li>
+                <li>About us</li>
+                <li>Contact us</li>
+            </div>
+                <img src="../resources/img/home.svg" alt="home"></img>
+            </div>
+            <div className="middleParagraph">
+                
+                <h1 className="subTitle">WELCOME BACK</h1>
+                <div className="hook">
+                <h2 className="hook_1">Don't have an account, </h2>
+                <h2 className="hook_2">Sign up</h2>
+                </div>
+                    <img className="logo" src="../resources/img/logo.png" alt="logo"></img>
+            </div>   
+           
+            
+
+        </div>
+    );
+}
+
+export default Login;
