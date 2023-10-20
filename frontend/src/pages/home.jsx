4a899b05fa0bc7ae754b9c71afde93cf2b131b7b
Neer-rn <neerrnbhatta@gmail.com>
2023-10-20 22:19:44 +0545

    first page made except photos

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./home.css";
+import '../resources/img/logo.png';
 
 const Home = () => {
     return (
         <div className="Home">
+            <div className="Up_bar">
+
             <div className="home_name">
-                <h1>KU-Bazar</h1>
+                <h1 className="title">KU-Bazar</h1>
 
             </div>
             <div className="home_list">
@@ -13,7 +16,27 @@ const Home = () => {
                 <li>About us</li>
                 <li>Contact us</li>
             </div>
-                <button className="button_signup">Sign up</button>    
+                <button className="button_signup">Sign up</button> 
+            </div>
+            <div className="middleParagraph">
+                
+                <h1 className="subTitle">Ku-Bazar</h1>
+                <div className="hook">
+                <h2 className="hook_1">Your One-Stop </h2>
+                <h2 className="hook_2"> , E-Store</h2>
+                </div>
+                    
+                    <p className="description"> Discover KU-Bazar: Your Hub for Smart Shopping<br></br>
+                                                in the KU Community. Buy, Sell, and Connect<br></br>
+                                                Seamlessly. Embrace Open-Source Convenience,<br></br>
+                                                No Fees Attached.Join Today!!<br></br>
+                    </p>
+
+                    <img className="xlogo" src="../resources/img/xlogo.svg" alt="xlogo"></img>
+            </div>   
+           
+            
+
         </div>
     );
 }
