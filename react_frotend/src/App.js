c08d5a1f555710575a70835ae379b3a1d613a0bf
Neer-rn <neerrnbhatta@gmail.com>
2023-10-28 21:00:44 +0545

    New react folder made also signup ui completed

---
+++ react_frotend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Signup from './pages/signup';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+     <Signup/>
+
+    </div>
+  );
+}
+
+export default App;
