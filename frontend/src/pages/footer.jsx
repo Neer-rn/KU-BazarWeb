c0510e6563d241562a85f5dc0c74bcd76de48d70
Neer-rn <neerrnbhatta@gmail.com>
2023-11-13 08:20:59 +0545

    footer completed

--- frontend/src/pages/footer.jsx
+++ frontend/src/pages/footer.jsx
@@ -1,30 +1,35 @@
 // Footer.jsx
 import React from 'react';
-//import './Footer.css'; // You can style your footer in a separate CSS file
+import './footer.css'; 
 
 const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-content">
       <div className="footer-section">
+      <div className='sub'>
           <h3>Subscribe to Our Newsletter</h3>
           <p>Get exclusive updates and offers straight to your inbox.</p>
           <div className="subscribe-form">
-            <input type="email" placeholder="Your email" /> <br/>
+            <input className='input_email' type="email" placeholder="Your email" /> <br/>
             <button>Subscribe</button>
+            </div>
           </div>
         <div className="footer-section">
+        <div className='sub'>
           <h3>Contact Information</h3>
           <p>
           Dhulikhel, Nepal
           <br />
-            Email: neerrn2552@gmail.com
+            neerrn2552@gmail.com
             <br />
-            Phone: +977 9761665790
+              +977 9761665790
           </p>
+          </div>
         </div>
         
         <div className="footer-section">
+        <div className='sub'>
           <h3>Account</h3>
           <ul>
             <li><a href="#">Home</a></li>
@@ -32,8 +37,24 @@ const Footer = () => {
             <li><a href="#">Signup/Login</a></li>
             <li><a href="#">Shop</a></li>
           </ul>
+          </div>
         </div>
 
+        <div className="footer-section">
+        <div className='sub'>
+          <h3>Download App</h3>
+          <div className="download-app">
+            <img src="google-play-store.png" alt="Google Play Store" />
+            <div className="qr-code">
+              <img src="qr-code.png" alt="QR Code" />
+            </div>
+          </div>
+          <div className="social-media">
+            <img src="twitter-logo.png" alt="Twitter" />
+            <img src="facebook-logo.png" alt="Facebook" />
+          </div>
+          </div>
+            </div>
       </div>
       <div className="footer-bottom">
         <p>&copy;  Copyright KU-Bazar 2023. All rights reserved.</p>
